901dbfde04b716fa6d7d0cc1f7883d4948822d3c
tomtex04 <tomtex04@gmail.com>
2025-03-24 22:42:29 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('            Ahoj!');
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server beží na http://localhost:${port}`);
+});
+
