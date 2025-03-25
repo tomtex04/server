8d3bc401381239b233e336a1f5e13eddfa58d7f1
tomtex04 <tomtex04@gmail.com>
2025-03-25 19:42:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Formular
 app.get('/', (req, res) => {
-    res.send('Ahoj!');
+    res.send(`
+        <form method="GET" action="/greet">
+            <input type="text" name="meno" placeholder="First name" />
+            <button type="submit">ENTER</button>
+        </form>
+    `);
 });
+
+// Spracovanie mena
+app.get('/greet', (req, res) => {
+    const meno = req.query.meno; 
+    res.send(`Ahoj ${meno}!`);
+});
+
 
 const server = app.listen(port, () => {
     console.log(`Server beží na http://localhost:${port}`);
